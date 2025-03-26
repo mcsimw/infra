5de70563b9593d197ee59ed70c04d5f3be24b4e4
Maor Haimovitz <maor@mcsimw.com>
2025-03-25 23:02:31 -0400

    no games

--- compootuers/perSystem/x86_64-linux/eldritch/both.nix
+++ compootuers/perSystem/x86_64-linux/eldritch/both.nix
@@ -5,24 +5,12 @@
     self.nixosModules.dwl
   ];
   environment.systemPackages = with pkgs; [
-    heroic
-    zeroad
-    mangohud
-    gamescope
     obs-studio
-    dolphin-emu
     zoom-us
     libreoffice-fresh
-    dwarf-fortress
     vesktop
     signal-desktop
   ];
-  programs = {
-    steam = {
-      enable = true;
-      extest.enable = true;
-    };
-    wireshark.enable = true;
-  };
+  programs.wireshark.enable = true;
   system.stateVersion = "25.05";
 }
