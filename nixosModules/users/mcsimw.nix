b54d871b9df583a59bb6478f9852aade2e7c73dc
Maor Haimovitz <maor@mcsimw.com>
2025-04-05 13:51:35 -0400

    yes

--- nixosModules/users/mcsimw.nix
+++ nixosModules/users/mcsimw.nix
@@ -2,11 +2,19 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
   cfg = config.myShit.users.mcsimw.enable;
   dwlEnabled = lib.attrByPath [ "myShit" "dwl" "enable" ] false config;
+  dwl = pkgs.writeShellScriptBin "dwl" ''
+    ${self'.packages.dwl}/bin/dwl -s "
+      dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_SESSION_TYPE XDG_CURRENT_DESKTOP=dwl;
+      systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_SESSION_TYPE;
+      systemctl --user start yes.target;
+    "
+  '';
 in
 {
   options.myShit.users.mcsimw.enable = lib.mkOption {
@@ -17,6 +25,12 @@ in
   };
 
   config = lib.mkIf cfg {
+    systemd.user.targets.yes = {
+      documentation = [ "man:systemd.special(7)" ];
+      bindsTo = [ "graphical-session.target" ];
+      wants = [ "graphical-session-pre.target" ];
+      after = [ "graphical-session-pre.target" ];
+    };
     users.users.mcsimw = {
       description = "Maor Haimovitz";
       isNormalUser = true;
@@ -25,13 +39,13 @@ in
       uid = 1000;
       initialHashedPassword = "$y$j9T$HmE1eeCA3RdENLRrDyjmC/$QROkFnFmJC18wgrAGu24j8EiCGTEv3N9oC7mN7aj9A8";
       packages =
-        with self'.packages;
         [
-          git
-          nvim
+          self'.packages.git
+          self'.packages.nvim
         ]
         ++ lib.optionals dwlEnabled [
-          foot
+          self'.packages.foot
+          dwl
         ];
     };
   };
