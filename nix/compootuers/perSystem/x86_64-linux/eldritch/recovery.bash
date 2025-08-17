6b4b82c38573f8e3b23d1d998be5c6521ac96a3b
Maor Haimovitz <maor@mcsimw.com>
2025-08-17 10:25:24 -0400

    small fix

--- nix/compootuers/perSystem/x86_64-linux/eldritch/recovery.bash
+++ nix/compootuers/perSystem/x86_64-linux/eldritch/recovery.bash
@@ -8,4 +8,4 @@ mount --mkdir -t zfs nyx/tmp /mnt/tmp -o zfsutil
 mount --mkdir -t zfs nyx/persist /mnt/persist -o zfsutil
 mount --mkdir -t vfat -o dmask=0022,fmask=0022,umask=0077 /dev/disk/by-partuuid/1842a05b-a2fa-4e8e-aa1c-a21d684f7087 /mnt/boot
 
-sudo nixos-install --no-channel-copy --no-root-passwd --flake ".#eldritch
+sudo nixos-install --no-channel-copy --no-root-passwd --flake ".#eldritch"
